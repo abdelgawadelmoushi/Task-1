32804e9823617f6b0770789ad1b28c6bd8810113
abdelgawadelmoushi <abdelgawadelmoushi@gmail.com>
2025-07-31 12:18:50 -0700

    Update Program.cs

--- task/Program.cs
+++ task/Program.cs
@@ -24,3 +24,38 @@ Console.WriteLine("=====================");
 Console.WriteLine($"Total estimate : {cost * 6.6 / 100+ cost :C} ");
 
 Console.WriteLine("This estimate is valid for 30 days");
+
+
+//------------------------------------------------------------------------
+
+
+
+
+// task 2  (Standard Numeric Format Strings)
+
+
+//int x = 10;
+//int y = 20;
+
+//Console.WriteLine($"equation : {x} + {y} = {x + y:C}");
+
+// the output of the equation i= $30.00 ? 
+//because we have used C to refer to the currency within the equation after getting the result of 10 + 20 which is x + y 
+
+
+//double price = 1234.56;
+
+//Console.WriteLine($"the price is  : {price:C}");
+
+
+
+//double percentage = 0.85;
+//Console.WriteLine(percentage.ToString("P")); // 85.00 %
+
+
+
+
+
+//int bytes = 1024;
+//Console.WriteLine($"Memory used: {bytes.ToString("X")} bytes"); // Memory used: 400 bytes
+
